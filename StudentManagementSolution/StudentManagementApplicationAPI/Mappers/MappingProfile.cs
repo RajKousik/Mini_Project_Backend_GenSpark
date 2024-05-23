25f5d7969e95071d13f793a844c00e9f44dca146
Kousik <rajkousik20@gmail.com>
2024-05-23 17:09:14 +0530

    Auto Mappers are created

---
+++ StudentManagementSolution/StudentManagementApplicationAPI/Mappers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using StudentManagementApplicationAPI.Models.Db_Models;
+using StudentManagementApplicationAPI.Models.DTOs.StudentDTOs;
+
+namespace StudentManagementApplicationAPI.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Student, StudentRegisterDTO>().ReverseMap();
+        }
+    }
+}
